a0fa787ce6ba6ebaa8e9c4975c63ee9092ba73a2
Anton Morozov <frostman13@mail.ru>
2017-09-02 18:25:14 +0300

    Удалил ненужные файлы

--- summ.py
+++
@@ -1,5 +0,0 @@
-one = input('Введите значение 1: ')
-two = input('Введите значение 2: ')
-def get_summ (one,two,delimetr=' '):
-	return str(one)+str(delimetr)+str(two)
-print(get_summ(one,two).upper())
