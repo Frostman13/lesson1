221ff05a5a2ffcb66f97e6b54d47ea8e2243923d
Anton Morozov <frostman13@mail.ru>
2017-09-02 17:03:39 +0300

    Первое сохранение

---
+++ answers.py
@@ -0,0 +1,9 @@
+# answers = {"привет": "И тебе привет!", "как дела": "Лучше всех", "пока": "Увидимся"}
+# print(answers['привет'])
+
+question = input().lower()
+def get_answer(question):
+    answers = {"привет": "И тебе привет!", "как дела": "Лучше всех", "пока": "Увидимся"}
+    return answers[question]
+print(get_answer(question))	
+
